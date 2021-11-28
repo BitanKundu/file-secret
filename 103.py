69b0e331cb02bd949cc9f29cbcb0421bf4a38710
BitanKundu <79818809+BitanKundu@users.noreply.github.com>
2021-11-27 20:58:07 -0800

    Add files via upload

---
+++ 103.py
@@ -0,0 +1,40 @@
+x="Sunday"
+for i in x :
+    print(i)
+
+y="Saturday" 
+for i in y :
+    print(i)
+
+    
+a="Friday"
+for i in a :
+    print(i)
+
+string="5"
+for i in string:
+    print (i)
+
+string="5"
+for d in string:
+    print(d)
+
+string="6"
+for g in string:
+    print(d)
+
+string="7"
+for d in string:
+    print(d)
+
+stirng="9"
+for a in string:
+    print(a)
+
+#strip function is used to remove any kind of trailing spaces that means any extra space from the start or the end
+#upper function is used to convert a string into upper case
+#lower function is used to convert a string to lower case
+#find function is used to search for a particular character or a set of characters in a string
+#strip
+string=" Monday "
+string=string.strip() #this will remove the starting and the ending space from the string
